1323ac08db859e56379ce43c3765a2694cdd5685
gdetam <hellostyle@mail.ru>
2022-04-19 17:51:37 +0300

    added comments to article

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser, Article
+from .models import Article, Comment, CustomUser
 
 
-admin.site.register(CustomUser)
 admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(CustomUser)
--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('articles/', views.ArticleListAPIView.as_view(), name='articles_list'),
     path('articles/<int:pk>/', views.ArticleDetailAPIView.as_view(), name='articles_detail'),
-    path('articles/create/', views.ArticleCreateAPIView.as_view(), name='articles_create'),
+    path('comments/create/', views.CommentCreateAPIView.as_view(), name='comments_create'),
     path('users/', views.UserListAPIView.as_view(), name='user_list'),
     path('users/create/', views.UserCreateAPIView.as_view(), name='user_create'),
     path('auth/', include('djoser.urls.authtoken')),
